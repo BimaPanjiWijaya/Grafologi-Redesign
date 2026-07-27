ad1d17575f327922968cd703dd8f4f28d1fcaeef
unknown <bimapanjiwijaya@gmail.com>
2026-07-27 20:35:55 +0700

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+document.getElementById("daftarBtn").addEventListener("click", function () {
+  alert(
+    "Tombol ini akan mengarah ke halaman pendaftaran Comprehensive Course (CHA).",
+  );
+});
